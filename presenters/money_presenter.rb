1cfb5fe94c5e5fcabe480df9cc19fba04505d3ff
RobertClayton <robbie.c.clayton@gmail.com>
2020-10-22 22:17:21 +0100

    Presenting Balance

---
+++ presenters/money_presenter.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# holds the logic for formatting and presenting a balance
+class MoneyPresenter
+  def present(balance)
+    "Balance: #{format_money(balance)}"
+  end
+
+  private
+
+  def format_money(balance)
+    "£#{"%.2f" % (balance.to_f / 100)}"
+  end
+end
--- vending_machine.rb
+++ vending_machine.rb
@@ -11,14 +11,19 @@ require_all 'presenters'
 
 # Entry class for the Vending Machine.
 class VendingMachine
-  attr_reader :balance, :cli, :inventory_controller, :money
+  attr_reader :cli, :inventory_controller, :money, :total_balance
 
   def initialize
+    @total_balance = 200
     @inventory_controller = InventoryInitialiser.default_load(InventoryController.new)
     @money = Money.new
   end
 
   def inventory
     InventoryTable.new.present(@inventory_controller.inventory)
+  end
+
+  def balance
+    MoneyPresenter.new.present(total_balance)
   end
 end
